4e7a33ebb002a0ed6516aae4b859e1f6d6e849c8
quoctanvn <quoctanvn@gmail.com>
2016-09-29 09:33:52 +0200

    Update DEPLOY.R

--- R-Script/DEPLOY.R
+++ R-Script/DEPLOY.R
@@ -2,4 +2,4 @@ install.packages("rsconnect")
 rsconnect::setAccountInfo(name='iesegmbd', token='80C670C6CCD30F2FA6A3B18847E73BD4', secret='POCQEv1EXomoU+SFtFjCoRa0ZxMDtgX0Eu3w1bKJ')
 
 library(rsconnect)
-rsconnect::deployApp("C:/Users/Mahir/Documents/IESEG/RecomenderSystems/Project/final")
+rsconnect::deployApp("C:/Users/Quoctan/Documents/IESEG/RecomenderSystems/Project/final")
