7fc0a4e4a6169110fae195fb7ac5a20fbfae71f9
quoctanvn <quoctanvn@gmail.com>
2016-09-29 09:29:45 +0200

    Create DEPLOY.R

---
+++ R-Script/DEPLOY.R
@@ -0,0 +1,5 @@
+install.packages("rsconnect")
+rsconnect::setAccountInfo(name='iesegmbd', token='80C670C6CCD30F2FA6A3B18847E73BD4', secret='POCQEv1EXomoU+SFtFjCoRa0ZxMDtgX0Eu3w1bKJ')
+
+library(rsconnect)
+rsconnect::deployApp("C:/Users/Mahir/Documents/IESEG/RecomenderSystems/Project/final")
